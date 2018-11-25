48dc81da304decffdb861ecfebf6bd06227abd90
SimplyRin <simplyrin.dev@gmail.com>
2018-11-25 23:32:03 +0900

    Add files via upload

---
+++ src/net/simplyrin/konomirollback/Main.java
@@ -0,0 +1,111 @@
+package net.simplyrin.konomirollback;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.bukkit.GameMode;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by SimplyRin on 2018/11/25.
+ *
+ * Copyright (c) 2018 SimplyRin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+public class Main extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		this.saveDefaultConfig();
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public void onDisable() {
+		this.log("Starting rollback...");
+		World world = this.getServer().getWorld("lobby");
+		File folder = world.getWorldFolder();
+		if (world != null) {
+			this.getServer().unloadWorld(world, true);
+		}
+		String backupFolderName = this.getConfig().getString("Backup-Lobby");
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			System.out.println("Resetting lobby...");
+			this.copyWorld(new File(backupFolderName), folder);
+		}));
+		this.log("Rollback done!");
+	}
+
+	public void log(String log) {
+		System.out.println("[KonomiRollback] " + log);
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		if (this.getConfig().getStringList("Auto-OP").contains(player.getUniqueId().toString())) {
+			player.setOp(true);
+		} else {
+			player.setOp(false);
+			player.setGameMode(GameMode.SURVIVAL);
+		}
+	}
+
+	public void copyWorld(File source, File target) {
+		try {
+			ArrayList<String> ignore = new ArrayList<String>(Arrays.asList("uid.dat", "session.dat"));
+			if (!ignore.contains(source.getName())) {
+				if (source.isDirectory()) {
+					if (!target.exists())
+					target.mkdirs();
+					String files[] = source.list();
+					for (String file : files) {
+						File srcFile = new File(source, file);
+						File destFile = new File(target, file);
+						this.copyWorld(srcFile, destFile);
+					}
+				} else {
+					InputStream in = new FileInputStream(source);
+					OutputStream out = new FileOutputStream(target);
+					byte[] buffer = new byte[1024];
+					int length;
+					while ((length = in.read(buffer)) > 0) {
+						out.write(buffer, 0, length);
+					}
+					in.close();
+					out.close();
+				}
+			}
+		} catch (IOException e) {
+		}
+	}
+
+}
